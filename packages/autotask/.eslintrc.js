91640571fae567ada27e125c25885590daca1c89
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-07-26 18:14:25 -0400

    chore: lint + cleanup + explicit versions

---
+++ packages/autotask/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parserOptions: {
+      "project": "./tsconfig.json",
+      "tsconfigRootDir": __dirname,
+      "sourceType": "module"
+  },
+  rules: {
+    "no-console": "off",
+    "import/prefer-default-export": "off",
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    "airbnb-typescript-prettier",
+  ],
+};
