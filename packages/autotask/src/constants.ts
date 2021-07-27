fbc38d749fb25e2acbc592972e1e062bb491deca
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-07-27 14:58:56 -0400

    fix: bug in USDC_CLAIMED_TOO_LOW

--- packages/autotask/src/constants.ts
+++ packages/autotask/src/constants.ts
@@ -2,4 +2,4 @@ import { BigNumber } from "@ethersproject/bignumber";
 
 export const USDC_DECIMALS = BigNumber.from(10).pow(6);
 export const UNISWAP_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-export const USDC_CLAIMED_TOO_LOW = BigNumber.from(500).pow(6);
+export const USDC_CLAIMED_TOO_LOW = USDC_DECIMALS.mul(500);
