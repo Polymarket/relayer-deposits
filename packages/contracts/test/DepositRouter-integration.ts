cc4e88d58f5388b92bb262246487ceaa7dea276c
Sam Hatem <sam.hatem17@gmail.com>
2021-08-03 12:07:12 -0400

    feat: integration test

--- packages/contracts/test/DepositRouter-integration.ts
+++ packages/contracts/test/DepositRouter-integration.ts
@@ -6,7 +6,13 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
 import { fundAccountETH, fundAccountUSDC } from "mainnet-fork-helpers";
 import { splitSignature } from "@ethersproject/bytes";
 import { JsonRpcSigner } from "@ethersproject/providers";
-import { getReceiveSignature, getEip3009Nonce, Signature, getContracts } from "@polymarket/relayer-deposits";
+import {
+    getReceiveSignature,
+    getEip3009Nonce,
+    Signature,
+    getContracts,
+    getDepositSignature,
+} from "@polymarket/relayer-deposits";
 
 import { DepositRouter } from "../typechain";
 import { deploy, getSignerFromWallet } from "./helpers";
@@ -14,10 +20,10 @@ import { deploy, getSignerFromWallet } from "./helpers";
 const { usdc, rootChainManager, usdcPredicate } = getContracts(1);
 
 const ONE_USDC = BigNumber.from(10).pow(6);
+const ONE_ETH = BigNumber.from(10).pow(18);
 
 const setup = deployments.createFixture(async () => {
     const admin = await ethers.getNamedSigner("admin");
-    const ONE_ETH = BigNumber.from(10).pow(18);
 
     const usdcContract = new ethers.Contract(
         usdc,
@@ -29,7 +35,7 @@ const setup = deployments.createFixture(async () => {
     await fundAccountUSDC(admin, ONE_ETH.mul(10), usdc);
 
     const router = await deploy<DepositRouter>("DepositRouter", {
-        args: [usdc, rootChainManager, usdcPredicate, admin.address, [admin.address]],
+        args: [usdc, rootChainManager, usdcPredicate, admin.address, ONE_ETH.mul(2)],
         connect: admin,
     });
 
@@ -46,29 +52,32 @@ describe("Integration tests", function () {
         let usdcContract: Contract;
         let admin: SignerWithAddress;
         let signer: JsonRpcSigner;
-        let reusedReceiveSig: Signature;
 
+        let validBefore: number;
+        let value: BigNumber;
         let fee: BigNumber;
-        let reusedValue: BigNumber;
-        let reusedNonce: string;
-        let reusedValidBefore: number;
+        let receiveNonce: string;
+        let receiveSig: Signature;
+        let depositSig: Signature;
+        let gasPrice: BigNumber;
 
-        before(async function () {
+        beforeEach(async function () {
             const deployment = await setup();
             router = deployment.router;
             usdcContract = deployment.usdcContract;
             admin = deployment.admin;
             signer = getSignerFromWallet(admin, 31337);
-        });
 
-        it("should execute the deposit", async function () {
-            const validBefore = Math.floor(Date.now() / 1000 + 3600);
+            validBefore = Math.floor(Date.now() / 1000 + 3600);
 
-            const value = ONE_USDC;
+            value = ONE_USDC;
             fee = ONE_USDC.div(10);
 
-            const nonce = await getEip3009Nonce(admin, usdc);
-            const receiveSig = splitSignature(
+            const registerTx = await router.register("some url", { value: ONE_ETH.mul(2) });
+            await registerTx.wait();
+
+            receiveNonce = await getEip3009Nonce(admin, usdc);
+            receiveSig = splitSignature(
                 await getReceiveSignature({
                     signer,
                     tokenName: "USD Coin",
@@ -77,49 +86,78 @@ describe("Integration tests", function () {
                     verifyingContract: usdc,
                     to: router.address,
                     value,
-                    nonce,
+                    nonce: receiveNonce,
                     validBefore,
                     validAfter: 0,
                 }),
             );
 
+            gasPrice = BigNumber.from(10).pow(9).mul(10); // 10 gwei gas price
+
+            const depositNonce = await router.nonces(admin.address);
+
+            depositSig = splitSignature(
+                await getDepositSignature({
+                    signer,
+                    chainId: 31337,
+                    verifyingContract: router.address,
+                    depositRecipient: admin.address,
+                    fee,
+                    gasPrice,
+                    nonce: depositNonce,
+                }),
+            );
+        });
+
+        it("should execute the deposit", async function () {
             const predicate = new ethers.Contract(
                 usdcPredicate,
                 ["event LockedERC20(address indexed, address indexed, address indexed, uint256 amount)"],
                 ethers.provider,
             );
 
-            await expect(router.deposit(admin.address, admin.address, value, fee, validBefore, nonce, receiveSig))
+            await expect(
+                router.deposit(
+                    admin.address,
+                    admin.address,
+                    value,
+                    fee,
+                    validBefore,
+                    receiveNonce,
+                    receiveSig,
+                    depositSig,
+                    { gasPrice },
+                ),
+            )
+                .to.emit(predicate, "LockedERC20")
+                .withArgs(router.address, admin.address, usdc, value.sub(fee));
+        });
+
+        it("able to make multiple deposits", async () => {
+            const predicate = new ethers.Contract(
+                usdcPredicate,
+                ["event LockedERC20(address indexed, address indexed, address indexed, uint256 amount)"],
+                ethers.provider,
+            );
+
+            await expect(
+                router.deposit(
+                    admin.address,
+                    admin.address,
+                    value,
+                    fee,
+                    validBefore,
+                    receiveNonce,
+                    receiveSig,
+                    depositSig,
+                    { gasPrice },
+                ),
+            )
                 .to.emit(predicate, "LockedERC20")
                 .withArgs(router.address, admin.address, usdc, value.sub(fee));
 
-            reusedReceiveSig = receiveSig;
-
-            reusedValue = value;
-            reusedNonce = nonce;
-            reusedValidBefore = validBefore;
-        });
-
-        it("DepositRouter contract has a balance of the fee", async () => {
-            const balance = await usdcContract.balanceOf(router.address);
-            expect(balance.toString()).to.equal(fee.toString());
-        });
-
-        it("initial transfer should revert if deposit doesn't succeed", async () => {
-            const expectBalanceIsFee = async (): Promise<void> => {
-                const routerBalance = await usdcContract.balanceOf(router.address);
-                expect(routerBalance.toString()).to.equal(fee.toString());
-            };
-
-            await expectBalanceIsFee();
-
-            const validBefore = Math.floor(Date.now() / 1000);
-
-            const value = ONE_USDC;
-
-            const nonce = await getEip3009Nonce(admin, usdc);
-
-            const receiveSig = splitSignature(
+            receiveNonce = await getEip3009Nonce(admin, usdc);
+            receiveSig = splitSignature(
                 await getReceiveSignature({
                     signer,
                     tokenName: "USD Coin",
@@ -128,29 +166,170 @@ describe("Integration tests", function () {
                     verifyingContract: usdc,
                     to: router.address,
                     value,
-                    nonce,
+                    nonce: receiveNonce,
                     validBefore,
                     validAfter: 0,
                 }),
             );
 
-            await expect(
-                router.deposit(admin.address, ethers.constants.AddressZero, value, fee, validBefore, nonce, receiveSig),
-            ).to.be.revertedWith("RootChainManager: INVALID_USER");
+            gasPrice = BigNumber.from(10).pow(9).mul(10); // 10 gwei gas price
 
-            await expectBalanceIsFee();
-        });
+            const depositNonce = await router.nonces(admin.address);
 
-        it("fails on reused signature", async () => {
+            depositSig = splitSignature(
+                await getDepositSignature({
+                    signer,
+                    chainId: 31337,
+                    verifyingContract: router.address,
+                    depositRecipient: admin.address,
+                    fee,
+                    gasPrice,
+                    nonce: depositNonce,
+                }),
+            );
+
             await expect(
                 router.deposit(
                     admin.address,
                     admin.address,
-                    reusedValue,
+                    value,
                     fee,
-                    reusedValidBefore,
-                    reusedNonce,
-                    reusedReceiveSig,
+                    validBefore,
+                    receiveNonce,
+                    receiveSig,
+                    depositSig,
+                    { gasPrice },
+                ),
+            )
+                .to.emit(predicate, "LockedERC20")
+                .withArgs(router.address, admin.address, usdc, value.sub(fee));
+        });
+
+        it("deposit router has a balance of the fee after the deposit", async () => {
+            const tx = await router.deposit(
+                admin.address,
+                admin.address,
+                value,
+                fee,
+                validBefore,
+                receiveNonce,
+                receiveSig,
+                depositSig,
+                { gasPrice },
+            );
+            await tx.wait();
+
+            const balance = await usdcContract.balanceOf(router.address);
+            expect(balance.toString()).to.equal(fee.toString());
+        });
+
+        it("initial transfer reverts if the deposit does not succeed", async () => {
+            const expectZeroBalance = async (): Promise<void> => {
+                const routerBalance = await usdcContract.balanceOf(router.address);
+                expect(routerBalance.toString()).to.equal(ethers.constants.Zero.toString());
+            };
+
+            await expectZeroBalance();
+
+            const depositNonce = await router.nonces(admin.address);
+
+            depositSig = splitSignature(
+                await getDepositSignature({
+                    signer,
+                    chainId: 31337,
+                    verifyingContract: router.address,
+                    depositRecipient: ethers.constants.AddressZero,
+                    fee,
+                    gasPrice,
+                    nonce: depositNonce,
+                }),
+            );
+
+            await expect(
+                router.deposit(
+                    admin.address,
+                    ethers.constants.AddressZero,
+                    value,
+                    fee,
+                    validBefore,
+                    receiveNonce,
+                    receiveSig,
+                    depositSig,
+                    { gasPrice },
+                ),
+            ).to.be.revertedWith("RootChainManager: INVALID_USER");
+
+            await expectZeroBalance();
+        });
+
+        it("fails on reused deposit signature", async () => {
+            const tx = await router.deposit(
+                admin.address,
+                admin.address,
+                value,
+                fee,
+                validBefore,
+                receiveNonce,
+                receiveSig,
+                depositSig,
+                { gasPrice },
+            );
+            await tx.wait();
+
+            await expect(
+                router.deposit(
+                    admin.address,
+                    admin.address,
+                    value,
+                    fee,
+                    validBefore,
+                    receiveNonce,
+                    receiveSig,
+                    depositSig,
+                    { gasPrice },
+                ),
+            ).to.be.revertedWith("DepositRouter::_verifyDepositSig: unable to verify deposit sig");
+        });
+
+        it("fails on reused receive signature", async () => {
+            const tx = await router.deposit(
+                admin.address,
+                admin.address,
+                value,
+                fee,
+                validBefore,
+                receiveNonce,
+                receiveSig,
+                depositSig,
+                { gasPrice },
+            );
+            await tx.wait();
+
+            const depositNonce = await router.nonces(admin.address);
+
+            depositSig = splitSignature(
+                await getDepositSignature({
+                    signer,
+                    chainId: 31337,
+                    verifyingContract: router.address,
+                    depositRecipient: admin.address,
+                    fee,
+                    gasPrice,
+                    nonce: depositNonce,
+                }),
+            );
+
+            await expect(
+                router.deposit(
+                    admin.address,
+                    admin.address,
+                    value,
+                    fee,
+                    validBefore,
+                    receiveNonce,
+                    receiveSig,
+                    depositSig,
+                    { gasPrice },
                 ),
             ).to.be.revertedWith("FiatTokenV2: authorization is used or canceled");
         });
