e6a5ac4ddc91299309d2c17fbe73f6749afcec8b
Sam Hatem <sam.hatem17@gmail.com>
2021-09-07 15:47:05 -0400

    fix: ensure USDC is swapped in past swap fails + other fixes

--- packages/autotask/src/constants.ts
+++ packages/autotask/src/constants.ts
@@ -2,4 +2,6 @@ import { BigNumber } from "ethers";
 
 export const UNISWAP_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-export const CLAIMABLE_FEES_THRESHOLD = BigNumber.from(100);
+const USDC_DECIMALS = BigNumber.from(10).pow(6);
+
+export const CLAIMABLE_FEES_THRESHOLD = BigNumber.from(1000).mul(USDC_DECIMALS);
